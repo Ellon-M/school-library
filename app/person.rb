69988edf67ba2da9ba2f38355c83fa96bc42f1ab
Ellon M <mordecaimwema11@gmail.com>
2023-03-10 13:35:55 +0300

    add associations between classes

--- app/person.rb
+++ app/person.rb
@@ -1,16 +1,17 @@
 require_relative 'nameable'
 
 class Person < Nameable
+  attr_reader :id, :rentals
+  attr_accessor :name, :age
+
   def initialize(age, name: 'Unknown', parent_permission: true)
     super()
     @permission = parent_permission
     @id = Random.rand(1..10_000)
     @name = name
     @age = age
+    @rentals = []
   end
-
-  attr_reader :id
-  attr_accessor :name, :age
 
   def correct_name
     @name

997955b7db6954cfaf1401a8fd8b2dd74d917404
Ellon M <mordecaimwema11@gmail.com>
2023-03-16 10:50:26 +0300

    fix rubocop linter errors

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -10,30 +10,30 @@ describe Person do
 
   describe '#age' do
     it 'returns the correct age' do
-      expect(@person_1.age).to eql 12
-      expect(@person_2.age).to eql 17
+      expect(@person_a.age).to eql 12
+      expect(@person_b.age).to eql 17
     end
   end
   describe '#name' do
     it 'returns the correct author' do
-      expect(@person_1.name).to eql 'Jane'
-      expect(@person_2.name).to eql 'Claudia'
+      expect(@person_a.name).to eql 'Jane'
+      expect(@person_b.name).to eql 'Claudia'
     end
   end
 
   context 'the class Person' do
     it 'returns true if he can use service for a person wtih age greater than 18' do
-      expect(@person_3.can_use_services?).to eq true
+      expect(@person_c.can_use_services?).to eq true
     end
 
     it 'returns false if he can use service for a person with age less than 18' do
-      expect(@person_2.can_use_services?).to eq false
+      expect(@person_b.can_use_services?).to eq false
     end
 
     it 'adds into rentals for a new book' do
       new_book = Book.new('House in the Cerulean Sea', 'TJ Klune')
-      @person_2.add_rental(new_book, '12/03/2023')
-      expect(@person_2.rentals.length).to eq 1
+      @person_b.add_rental(new_book, '12/03/2023')
+      expect(@person_b.rentals.length).to eq 1
     end
   end
 end
